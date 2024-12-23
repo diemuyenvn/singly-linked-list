34a86588a628b5e364917872a5724792afdc7206
diemuyenvn <diemuyenvn25355829907712003421@gmail.com>
2024-12-23 20:54:25 +0700

    Create SinglyLinkedList.java

---
+++ SinglyLinkedList.java
@@ -0,0 +1,86 @@
+package org.example;
+
+public class SinglyLinkedList<E> {
+    private static class Node<E> {
+        private final E element;
+        private Node<E> next;
+
+        public Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public E getElement() {
+            return this.element;
+        }
+
+        public Node<E> getNext() {
+            return this.next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public SinglyLinkedList() {
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public E getFirst() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.head.getElement();
+    }
+
+    public E getLast() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        return this.tail.getElement();
+    }
+
+    public void addFirst(E element) {
+        this.head = new Node<>(element, this.head);
+        if (this.isEmpty()) {
+            this.tail = this.head;
+        }
+        size++;
+    }
+
+    public void addLast(E element) {
+        Node<E> newNode = new Node<>(element, null);
+        if (this.isEmpty()) {
+            this.head = newNode;
+        } else {
+            tail.setNext(newNode);
+        }
+        tail = newNode;
+        size++;
+    }
+
+    public E removeFirst() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        E element = this.head.getElement();
+        this.head = this.head.getNext();
+        size--;
+        if (this.isEmpty()) {
+            this.tail = null;
+        }
+        return element;
+    }
+}
